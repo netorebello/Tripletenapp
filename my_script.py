3af3da1d48fcc7a38d7e9220eb99656a490736c4
Neto Rebello <hneto8@hotmail.com>
2024-07-09 22:19:07 +0100

    1.0

--- my_script.py
+++ my_script.py
@@ -1,1 +1,6 @@
-print('Olá')
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles.csv') # lendo os dados
+fig = px.histogram(car_data, x="odometer") # criar um histograma
+fig.show()
