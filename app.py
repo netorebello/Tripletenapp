040b422e4da7a66e16244f62cc062884398b341a
Neto Rebello <hneto8@hotmail.com>
2024-07-11 21:58:14 +0100

    Projeto final, Teste preliminar 2.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 data = pd.read_csv('vehicles.csv') # lendo os dados
 st.header('App para análise de dados de carros anunciados') #cabeçalho
 
-# Função para incluir fabricantes com menos de 1000 anúncios
+# Função para incluir Modelos com menos de 1000 anúncios
 def filter_manufacturers(data, threshold=1000):
     manufacturer_counts = data['model'].value_counts()
     filtered_manufacturers = manufacturer_counts[manufacturer_counts < threshold].index
@@ -14,17 +14,19 @@ def filter_manufacturers(data, threshold=1000):
 # Visualizador de Dados
 st.subheader('Visualizador de Dados')
 
-if st.button('Incluir Fabricantes com Menos de 1000 Anúncios'):
+incluir_fabricantes = st.checkbox('Incluir Modelos com Menos de 1000 Anúncios')
+
+if incluir_fabricantes:
     filtered_data = filter_manufacturers(data)
     st.write(filtered_data)
 else:
     st.write(data)
 
-# Tipos de Veículos por Fabricante
-st.subheader('Tipos de Veículos por Fabricante')
+# Tipos de Veículos por Modelo
+st.subheader('Tipos de Veículos por Modelo')
 
 vehicle_types_by_manufacturer = data.groupby(['model', 'type']).size().reset_index(name='counts')
-fig_vehicle_types = px.bar(vehicle_types_by_manufacturer, x='model', y='counts', color='type', title='Tipos de Veículos por Fabricante')
+fig_vehicle_types = px.bar(vehicle_types_by_manufacturer, x='model', y='counts', color='type', title='Tipos de Veículos por Modelo')
 st.plotly_chart(fig_vehicle_types)
 
 # Histograma de Condição Vs Ano do Modelo
@@ -33,18 +35,19 @@ st.subheader('Histograma de Condição Vs Ano do Modelo')
 fig_condition_model_year = px.histogram(data, x='model_year', color='condition', title='Condição Vs Ano do Modelo')
 st.plotly_chart(fig_condition_model_year)
 
-# Comparador de Preço entre Fabricantes
-st.subheader('Comparador de Preço entre Fabricantes')
+# Comparador de Preço entre Modelos
+st.subheader('Comparador de Preço entre Modelos')
 
 manufacturers = data['model'].unique()
-manufacturer_1 = st.selectbox('Selecione o Primeiro Fabricante', manufacturers)
-manufacturer_2 = st.selectbox('Selecione o Segundo Fabricante', manufacturers)
+manufacturer_1 = st.selectbox('Selecione o Primeiro Modelo', manufacturers)
+manufacturer_2 = st.selectbox('Selecione o Segundo Modelo', manufacturers)
 
 if manufacturer_1 and manufacturer_2:
     filtered_data_1 = data[data['model'] == manufacturer_1]
     filtered_data_2 = data[data['model'] == manufacturer_2]
 
-    fig_price_comparison = px.scatter(filtered_data_1, x='odometer', y='price', color=px.Constant(manufacturer_1), labels={'color': 'Fabricante'}, title='Comparador de Preço entre Fabricantes')
-    fig_price_comparison.add_scatter(x=filtered_data_2['odometer'], y=filtered_data_2['price'], mode='markers', name=manufacturer_2)
+    fig_price_comparison_1 = px.histogram(filtered_data_1, x='price', title=f'Preços do {manufacturer_1}')
+    fig_price_comparison_2 = px.histogram(filtered_data_2, x='price', title=f'Preços do {manufacturer_2}')
 
-    st.plotly_chart(fig_price_comparison)
+    st.plotly_chart(fig_price_comparison_1)
+    st.plotly_chart(fig_price_comparison_2)
